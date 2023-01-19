b12fe3618a98b25904aca8b206c70e7c07e794c1
reeganbenny <reeganbenny@gmail.com>
2023-01-19 17:19:24 +0530

    Frontend for location API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [status, setStatus] = useState(false);
+  const [watchId, setWatchId] = useState(0);
+  const [position, setPosition] = useState({});
+  const api = "https://locationbackend.onrender.com/location/add-location";
+  // const api = "http://localhost:3030/location/add-location";
+  useEffect(() => {
+    console.log("Status Changed to ", status);
+    if (status) {
+      setWatchId(
+        navigator.geolocation.watchPosition(
+          (position) => {
+            const { latitude, longitude } = position.coords;
+            console.log("latitude - ", latitude, " longitude - ", longitude);
+            setPosition({
+              latitude: latitude,
+              longitude: longitude,
+            });
+          },
+          (error) => {
+            console.log("Error- ", error);
+          }
+        )
+      );
+    } else if (watchId > 0) {
+      navigator.geolocation.clearWatch(watchId);
+      setWatchId(0);
+    }
+  }, [status]);
+
+  useEffect(() => {
+    if (status) {
+      console.log("Position", position);
+      fetch(api, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          watchId: watchId,
+          longitude: position.longitude,
+          latitude: position.latitude,
+          speed: position.speed,
+        }),
+      }).then((res) => {
+        console.log(res);
+      });
+    }
+  }, [position]);
+
+  const handleStatus = () => {
+    setStatus(!status);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="status">
+        <button onClick={handleStatus}>{status ? "Stop" : "Start"}</button>
+      </div>
     </div>
   );
 }
